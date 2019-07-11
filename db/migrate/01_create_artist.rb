2b3847f3f4e88080394cd82c3933a57e3a815a43
Craig Donavin <crdonavin@gmail.com>
2019-07-11 01:15:12 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artist.rb
+++ db/migrate/01_create_artist.rb
@@ -7,6 +7,12 @@ class CreateArtist < ActiveRecord::Migration
   end
 
   def change
+    create_table :artist do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    
   end
 
 end
